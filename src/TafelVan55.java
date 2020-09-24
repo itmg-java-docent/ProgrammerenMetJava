fc07f8dab722ee79c93345fd86852b68b525eaac
oraculo-nl <info@oraculo.nl>
2020-09-24 11:38:07 +0200

    first commit

---
+++ src/TafelVan55.java
@@ -0,0 +1,15 @@
+public class TafelVan55 {
+    public static void main(String[] args) {
+        var getal = 55;
+        System.out.println("1 x "+getal+" = "+getal*1);
+        System.out.println("2 x "+getal+" = "+getal*2);
+        System.out.println("3 x "+getal+" = "+getal*3);
+        System.out.println("4 x "+getal+" = "+getal*4);
+        System.out.println("5 x "+getal+" = "+getal*5);
+        System.out.println("6 x "+getal+" = "+getal*6);
+        System.out.println("7 x "+getal+" = "+getal*7);
+        System.out.println("8 x "+getal+" = "+getal*8);
+        System.out.println("9 x "+getal+" = "+getal*9);
+        System.out.println("10 x "+getal+" = "+getal*10);
+    }
+}
---
+++ src/TafelVan8.java
@@ -0,0 +1,14 @@
+public class TafelVan8 {
+    public static void main(String[] args) {
+        System.out.println("1 x 8 = 8");
+        System.out.println("2 x 8 = 16");
+        System.out.println("3 x 8 = 24");
+        System.out.println("4 x 8 = 32");
+        System.out.println("5 x 8 = 40");
+        System.out.println("6 x 8 = 48");
+        System.out.println("7 x 8 = 56");
+        System.out.println("8 x 8 = 64");
+        System.out.println("9 x 8 = 72");
+        System.out.println("10 x 8 = 80");
+    }
+}
